db613c5d8a1c014bbdfd95bc2afff24817976fc6
Christine Oen <oen.christine@gmail.com>
2014-04-23 11:46:20 -0500

    new file

---
+++ sum_square_diff.rb
@@ -0,0 +1,13 @@
+sum_square = 0
+(1..100).each do |x|
+	sum_square += x**2
+end
+
+square_sum = 0
+sum = 0
+(1..100).each do |x|
+	sum += x
+	square_sum = sum**2
+end
+
+puts square_sum - sum_squares
